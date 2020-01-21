bade676b7b4eb360a7d3615c32387733e91e31ab
kelypso <kklowlow@gmail.com>
2020-01-21 14:53:10 -0700

    update user controller comments; add mock login to app controller; create react app client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    # Mocked version of "logged in" user
+    helpers do 
+        def current_user 
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
