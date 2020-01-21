48bd47ae4ca732c81e05586c3e6f93bd34ec9d3d
kelypso <kklowlow@gmail.com>
2020-01-21 15:37:03 -0700

    add combineReducers

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware, compose} from 'redux';
-import {thunk} from 'redux-thunk';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
+
+const users = () => []
+const reducer = combineReducers({
+    users
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
