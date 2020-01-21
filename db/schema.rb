2f1e9bbb08cb3de058a3666b7f9f58fde319d77c
kelypso <kklowlow@gmail.com>
2020-01-21 13:31:07 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_023447) do
+ActiveRecord::Schema.define(version: 2020_01_21_201723) do
 
   create_table "entries", force: :cascade do |t|
     t.string "date"
