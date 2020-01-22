6f991162f1d04a976156e799781d71aff4049bdd
kelypso <kklowlow@gmail.com>
2020-01-21 19:32:09 -0700

    add get current user to routes, sessions, login actions, and app component did mount

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -12,4 +12,14 @@ class Api::V1::SessionsController < ApplicationController
         end
       end
 
+    def get_current_user
+        if logged_in? 
+            render json: current_user
+        else
+            render json: {
+              error: "User Not Logged In"
+            }
+        end
+    end
+
 end
--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -7,8 +7,8 @@ export const setCurrentUser = user => {
 }
 
 // Asynch action creators
-export const login = (credentials) => {
-    console.log('credentials are:',  credentials)
+export const login = credentials => {
+    //console.log('credentials are:',  credentials)
     return dispatch => {
       return fetch("http://localhost:3001/api/v1/login", {
         method: "POST",
@@ -16,6 +16,26 @@ export const login = (credentials) => {
           "Content-Type": "application/json"
         },
         body: JSON.stringify(credentials)
+      })
+        .then(resp => resp.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+      return fetch("http://localhost:3001/api/v1/get_current_user", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
       })
         .then(resp => resp.json())
         .then(user => {
