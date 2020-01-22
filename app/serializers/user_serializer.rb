6157489a75c8bab3a61ff26dab00739215b2632f
kelypso <kklowlow@gmail.com>
2020-01-22 14:04:28 -0700

    comment trips out of user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,14 +4,14 @@ class UserSerializer
   # has_many :trips, serializer: TripSerializer
   # has_many :entries, serializer: EntrySerializer
 
-  attribute :trips do |user|
-    user.trips.map do |trip|
-      {
-        name: trip.name,
-        category: trip.category,
-        duration: trip.duration.split(/, /).join(" - ")
-      }
-    end
-  end
+  # attribute :trips do |user|
+  #   user.trips.map do |trip|
+  #     {
+  #       name: trip.name,
+  #       category: trip.category,
+  #       duration: trip.duration.split(/, /).join(" - ")
+  #     }
+  #   end
+  # end
 
 end
