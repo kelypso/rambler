67330ddaaac2336013e621a626a0acac18ec501c
kelypso <kklowlow@gmail.com>
2020-01-21 20:55:55 -0700

    add status to session logout; add logout to app component

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::SessionsController < ApplicationController
         session.clear
         render json: {
             notice: "Successfully Logged Out"
-        }
+        }, status: 200
     end
 
 end
