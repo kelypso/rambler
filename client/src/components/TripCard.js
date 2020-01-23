ece103a52e738c05f92ad20cd081da715d7bb300
kelypso <kklowlow@gmail.com>
2020-01-23 13:46:30 -0700

    add link and route to trip show page on trip cards

--- client/src/components/TripCard.js
+++ client/src/components/TripCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const TripCard = ({trip}) => {
     return (
         trip ?
             <div className="TripCard">
-                <h2>{trip.attributes.name}</h2>
+                <h2><Link to={`/trips/${trip.id}`} key={trip.id}>{trip.attributes.name}</Link></h2>
                 <p>{trip.attributes.category}</p>
                 <p>{trip.attributes.duration}</p>
             </div> :
-        <p>No trips added yet!</p>
+        <p>Trip could not be found</p>
     )
 }
 
