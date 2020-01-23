55bfea43b476a847afa2f702420a35e9fc7ab3d1
kelypso <kklowlow@gmail.com>
2020-01-22 18:35:34 -0700

    remove implicit return from home comp

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,12 @@ import Login from './Login.js'
 import Logout from './Logout.js'
 import {Link} from 'react-router-dom'
 
-const Home = () => (
-    <div className="Home">
-        <Link to="/signup">Sign Up</Link> | <Link to="login">Login</Link>
-    </div>
-)
+const Home = () => {
+    return (
+        <div className="Home">
+            <Link to="/signup">Sign Up</Link> | <Link to="login">Login</Link>
+        </div>
+    )
+}
 
 export default Home
