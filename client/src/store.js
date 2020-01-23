b0d942280416f1a3feddfeef09605fa6f8717ab0
kelypso <kklowlow@gmail.com>
2020-01-22 20:19:35 -0700

    update order of items in store

--- client/src/store.js
+++ client/src/store.js
@@ -3,8 +3,8 @@ import thunk from 'redux-thunk'
 import user from './reducers/user.js'
 import loginForm from './reducers/loginForm.js'
 import signupForm from './reducers/signupForm.js'
-import trips from './reducers/trips.js'
 import newTripForm from './reducers/newTripForm.js'
+import trips from './reducers/trips.js'
 
 const reducer = combineReducers({
     user,
