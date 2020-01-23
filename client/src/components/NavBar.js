9108123ba4e79ef193e0bb84d545e46424155ad0
kelypso <kklowlow@gmail.com>
2020-01-22 20:48:34 -0700

    adding styling to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,9 +7,12 @@ const NavBar = ({user, loggedIn}) => {
     return (
         <div className="NavBar">
             {user ? <strong>Welcome, {user.attributes.name}</strong> : <strong>Welcome to Rambler</strong>}
-            <NavLink to="/trips">Trips</NavLink>
-            <NavLink to="/trips/new">New Trip</NavLink>
-            {loggedIn ? <Logout /> : null}
+            <span className="navLinks">
+                <NavLink exact activeClass to="/trips">  |  Trips  |  </NavLink>
+                <NavLink exact activeClass to="/trips/new">New Trip  |  </NavLink>
+            </span>
+            <span className="logout">{loggedIn ? <Logout /> : null}</span>
+            
         </div>
     )
 }
