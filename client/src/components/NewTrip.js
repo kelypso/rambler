f77250c327557e335c038799092a4311d71e05b4
kelypso <kklowlow@gmail.com>
2020-01-22 19:48:57 -0700

    add handle change to new trips

--- client/src/components/NewTrip.js
+++ client/src/components/NewTrip.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
-//import {updateNewTripForm} from '../actions/newTripForm.js'
+import {updateNewTripForm} from '../actions/newTripForm.js'
 // import {addTrip} from '../actions/user.js'
 
 const NewTrip = ({tripData, updateNewTripForm, addTrip, history}) => {
 
     const handleChange = e => {
+        e.preventDefault()
         const {name, value} = e.target
-        const updatedForm = {
-            ...tripData,
-            [name]: value
-        }
-        updateNewTripForm(updatedForm)
+        updateNewTripForm(name, value)
     }
 
     // const handleSubmit = e => {
@@ -22,9 +19,9 @@ const NewTrip = ({tripData, updateNewTripForm, addTrip, history}) => {
     return (
         <div className="NewTrip">
             <form>
-                <input type="text" name="name" onChange={handleChange} value={tripData.name} placeholder="name" /><br />
-                <input type="text" name="category" onChange={handleChange} value={tripData.category} placeholder="category" /><br />
-                <input type="text" name="duration" onChange={handleChange} value={tripData.duration} placeholder="start date - end date" /><br />
+                <input type="text" name="name" onChange={handleChange} placeholder="name" /><br />
+                <input type="text" name="category" onChange={handleChange} placeholder="category" /><br />
+                <input type="text" name="duration" onChange={handleChange} placeholder="start date - end date" /><br />
             <input type="submit" value="Add Trip" />
         </form>
         </div>
@@ -37,4 +34,4 @@ const NewTrip = ({tripData, updateNewTripForm, addTrip, history}) => {
 //     }
 // }
 
-export default NewTrip
+export default connect(null, {updateNewTripForm})(NewTrip)
