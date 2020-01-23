55199798441d5e95236584542fba9727f761fa3f
kelypso <kklowlow@gmail.com>
2020-01-22 18:42:58 -0700

    remove inconsistent ; from app, index, and store imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import {connect} from 'react-redux';
-import {getCurrentUser} from './actions/user.js';
-import NavBar from './components/NavBar.js';
-import Home from './components/Home.js';
-import Login from './components/Login.js';
-import Signup from './components/Signup.js';
-import Trips from './components/Trips.js';
-import MainContainer from './components/MainContainer.js';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import {connect} from 'react-redux'
+import {getCurrentUser} from './actions/user.js'
+import NavBar from './components/NavBar.js'
+import Home from './components/Home.js'
+import Login from './components/Login.js'
+import Signup from './components/Signup.js'
+import Trips from './components/Trips.js'
+import MainContainer from './components/MainContainer.js'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends React.Component {
   
