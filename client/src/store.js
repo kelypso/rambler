989ce7e22b1fb780c7ec8ae4b74317c1c75fc76f
kelypso <kklowlow@gmail.com>
2020-01-22 18:40:59 -0700

    remove unnecessary users from redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,11 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-// import users from './reducers/users.js'
 import user from './reducers/user.js'
 import loginForm from './reducers/loginForm.js'
 import signupForm from './reducers/signupForm.js'
 import trips from './reducers/trips.js'
 
 const reducer = combineReducers({
-    // users,
     user,
     loginForm,
     signupForm,
